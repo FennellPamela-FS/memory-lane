4d7e7f9219a0bd0584cd80382ec6b039bb2127cd
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-01-16 20:44:04 -0500

    Update error with cfebad1 build on deployment

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Blog from '../Components/Blog'
+import Blog from '../components/Blog'
 import styles from '../styles/Home.module.css'
 import { Inter } from '@next/font/google'
 
