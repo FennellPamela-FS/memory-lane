3709300655fe0a1f472d06f47c5c737c5e60de66
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-01-16 20:16:34 -0500

    Update index, removed lib/Posts.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Blog from '../components/Blog'
 import styles from '../styles/Home.module.css'
 import { Inter } from '@next/font/google'
+import Blog from '@/Components/Blog'
 
 // import styles from '@/styles/Home.module.css'
 // import Blog from '@/components/blog'
@@ -20,52 +20,52 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      < className={styles.main}>
 
-        <div className={styles.center}>
-          <h1 className="text-3xl tracking-wide uppercase font-bold text-violet-900 underline">
-            The
-          </h1>
-          <Image
-            className={styles.logo}
-            src="/pamLogo-pro.png"
-            alt="Pam Logo"
-            width={100}
-            height={100}
-            priority
-          />
-          <h1 className="text-3xl tracking-wide uppercase font-bold text-green-800 underline">
-            Blog
-          </h1>
+      <div className={styles.center}>
+        <h1 className="text-3xl tracking-wide uppercase font-bold text-violet-900 underline">
+          The
+        </h1>
+        <Image
+          className={styles.logo}
+          src="/pamLogo-pro.png"
+          alt="Pam Logo"
+          width={100}
+          height={100}
+          priority
+        />
+        <h1 className="text-3xl tracking-wide uppercase font-bold text-green-800 underline">
+          Blog
+        </h1>
+      </div>
+
+      <Blog />
+
+      <div className={styles.description}>
+        <p>
+          1.9 Assignment: &nbsp;
+          <code className={styles.code}>Memory Lane</code>
+        </p>
+        <div>
+          <a
+            href="https://github.com/FennellPamela-FS"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/pamLogo-pro-sm.svg"
+              alt="Vercel Logo"
+              className={styles.pamLogo}
+              width={24}
+              height={24}
+              priority
+            /> PamIam
+          </a>
         </div>
+      </div>
 
-        <Blog />
-
-        <div className={styles.description}>
-          <p>
-            1.9 Assignment: &nbsp;
-            <code className={styles.code}>Memory Lane</code>
-          </p>
-          <div>
-            <a
-              href="https://github.com/FennellPamela-FS"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/pamLogo-pro-sm.svg"
-                alt="Vercel Logo"
-                className={styles.pamLogo}
-                width={24}
-                height={24}
-                priority
-              /> PamIam
-            </a>
-          </div>
-        </div>
-
-      </main>
+    </main>
     </>
   )
 }
